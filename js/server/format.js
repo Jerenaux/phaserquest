64693021cd328bc13a10c05b7024def24646cde1
Jerome <jerome.renaux@gmail.com>
2017-06-07 15:22:21 +0200

    Adding "flattening" script for maps

--- js/server/format.js
+++ js/server/format.js
@@ -2,7 +2,6 @@
  * Created by Jerome on 23-08-16.
  */
 
-// https://github.com/bjorn/tiled/wiki/JSON-Map-Format
 var fs = require('fs');
 var clone = require('clone');
 
@@ -22,45 +21,46 @@ function Layer(w,h,name,level) {
 }
 
 function tileMap(map,isClient){
-    // Common to both
     this.height = map.height;
     this.width = map.width;
     this.tilewidth = map.tilewidth;
     this.tileheight = map.tileheight;
     this.layers = [];
-    this.tilesets = [map.tilesets[0]];
-    if(isClient){
+    this.tilesets = [map.tilesets[0]]; // for PQ we use only one tileset, the first one of the original two
+    if(isClient){ // the following is only useful for the client-side map
         this.orientation = map.orientation; // iso or ortho ; mandatory
         this.properties = map.properties;
     }
 }
-// Break map: don't forget the doors and entities (and later, music)
+
 function formatMap(){
     console.log('Formatting ...');
     var path = '/../../assets/maps/';
     var name = "map";
 
+    // Look by default for map.json in assets/map ; map.json is the exported JSON file from Tiled
     fs.readFile(__dirname+path+name+".json", 'utf8', function (err, data) {
         if (err) throw err;
         var map = JSON.parse(data);
-        // Copy a few properties from the initial map object into a new one
+        // Create two map representations, one for client and one for server, and copy a few properties from the initial map object into a new one
         var clientmap = new tileMap(map,true);
         var servermap = new tileMap(map,false);
 
+        // the ground-level layers display below the game entities, the high-level layers display above
         var newLayers = [new Layer(map.width,map.height,"layer0","ground")]; // array of ground-level layers
         var highLayers = [new Layer(map.width,map.height,"highlayer0","high")]; // array of layers appearing above entities
         var clientObjectLayers = [];
         var serverObjectLayers = [];
-        // Fill the layers with 0's
+        // Fill the layers with 0's (empty tiles)
         fillLayer(highLayers[0], map.width * map.height);
         fillLayer(newLayers[0],map.width*map.height);
-        for(var i= 0; i < map.layers.length; i++) { // Scan all layers one by one
+        for(var i= 0; i < map.layers.length; i++) { // Scan all layers of the original map one by one
             var layer = map.layers[i];
             if (layer.type === "tilelayer") {
                 //console.log('processing ' + layer.name);
                 for (var j = 0; j < layer.data.length; j++) { // Scan all tiles one by one
                     var tileProperties = map.tilesets[0].tileproperties[layer.data[j]-1];
-                    if(tileProperties && tileProperties.hasOwnProperty('v')){
+                    if(tileProperties && tileProperties.hasOwnProperty('v')){ // The original Browserquest map uses the property 'v' to indicate tiles that should be displayed above the player
                         addTile(highLayers,true,j,layer.data[j],map.width,map.height);
                     }else{
                         addTile(newLayers,false,j,layer.data[j],map.width,map.height);
@@ -68,7 +68,7 @@ function formatMap(){
                 }
                 //console.log('done with layer ' + layer.name);
             } else if (layer.type === "objectgroup") {
-                if(layer.name == 'doors' || layer.name == 'entities') {
+                if(layer.name == 'doors' || layer.name == 'entities') { // Only store doors and entities objects in client map
                     if(layer.name == 'entities'){
                         clientObjectLayers.push(filterEntities(clone(layer)));
                     }else {
@@ -79,12 +79,12 @@ function formatMap(){
             }
         }
 
-        countTiles(newLayers);
-        countTiles(highLayers);
-        //findTiles(newLayers[4]);
+        //countTiles(newLayers);
+        //countTiles(highLayers);
 
         console.log(clientObjectLayers.length+' client object layers');
         console.log(serverObjectLayers.length+' server object layers');
+        // Put all layers (objects and tiles) together
         clientmap.layers = newLayers.concat(highLayers).concat(clientObjectLayers);
         servermap.layers = newLayers.concat(highLayers).concat(serverObjectLayers);
 
@@ -100,6 +100,22 @@ function formatMap(){
 
     });
 }
+
+function addTile(layerArray,high,index,tile,w,h){ // Add a tile to the 'bottom-most' possible layer, or create one if necessary
+    if(tile == 0) return;
+    var depth = 0;
+    // Look for the first layer wih an empty tile at the corresponding position (=index)
+    while (layerArray[depth].data[index] != 0 && layerArray[depth].data[index] !== undefined) { // If there is already a tile in that position (index) at the current layer depth ...
+        depth++; // If non-empty, increase depth = look one layer further ("higher")
+        if (depth >= layerArray.length) { // If reached max depth, create new layer
+            var name = (high ? "highlayer" : "layer") + depth;
+            layerArray.push(new Layer(w,h, name,(high ? "high" : "ground")));
+            fillLayer(layerArray[depth], w*h);
+        }
+    }
+    layerArray[depth].data[index] = tile;
+}
+
 function countTiles(arr){
     for(var i = 0; i < arr.length; i++){
         var tmp = arr[i].data.slice();
@@ -112,7 +128,7 @@ function countTiles(arr){
     }
 }
 
-function findTiles(layer){
+/*function findTiles(layer){
     for(var i = 0; i < layer.data.length; i++){
         if(layer.data[i] > 0){
             var x = i%layer.width;
@@ -120,22 +136,7 @@ function findTiles(layer){
             console.log('tile at '+x+', '+y);
         }
     }
-}
-
-function addTile(layerArray,high,index,tile,w,h){
-    if(tile == 0) return;
-    var depth = 0;
-    // Look for the first layer wih an empty tile at the corresponding position (=index)
-    while (layerArray[depth].data[index] != 0 && layerArray[depth].data[index] !== undefined) {
-        depth++; // If non-empty, increase depth = look one layer further
-        if (depth >= layerArray.length) { // If reached max depth, create new layer
-            var name = (high ? "highlayer" : "layer") + depth;
-            layerArray.push(new Layer(w,h, name,(high ? "high" : "ground")));
-            fillLayer(layerArray[depth], w*h);
-        }
-    }
-    layerArray[depth].data[index] = tile;
-}
+}*/
 
 function fillLayer(layer,n){
     for(var k = 0; k < n; k++){
@@ -143,7 +144,7 @@ function fillLayer(layer,n){
     }
 }
 
-function filterEntities(layer){
+function filterEntities(layer){ // Filters out game entities that don't need to be present in the map, based on their ID in the tileset used to represent them
    var tmpobj = [];
     for(var i = 0; i < layer.objects.length; i++){
         var obj = layer.objects[i];
@@ -157,7 +158,7 @@ function filterEntities(layer){
     return layer;
 }
 
-function breakMap(map){
+/*function breakMap(map){
     var path = '/../../assets/maps/';
     var AOIwidth = 34; // 6 AOIs horizontally
     var AOIheight = 20; // 16 AOIs vertically
@@ -196,7 +197,7 @@ function breakMap(map){
             //console.log('Piece written');
         });
     }
-}
+}*/
 
 module.exports.format = formatMap;
 
